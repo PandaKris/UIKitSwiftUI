1f70fc97049af2cd84fcd4eddf4076158cd5c66b
Kristanto Sean <kristantosean@gmail.com>
2024-05-21 14:11:45 +0700

    Initial Commit

---
+++ UIKitSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIKitSwiftUI
+//
+//  Created by Kristanto Sean on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIKitSwiftUI/UIKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitSwiftUIApp.swift
+//  UIKitSwiftUI
+//
+//  Created by Kristanto Sean on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UIKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
